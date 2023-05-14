35a1c74c6f501e208fef2d2739e7a1f61ede0b40
Mou Biswas <qtmeaowmou16@gmail.com>
2023-05-14 23:59:18 +0600

    remote data source

--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,13 +1,43 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:number_trivia/core/error/exception.dart';
+
 import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaRemoteDataSource {
-  /// Calls the http://numbersapi.com/{number} endpoint.
-  ///
-  /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getConreteNumberTrivia(int number);
-
-  /// Calls the http://numbersapi.com/random endpont.
-  ///
-  /// Throws a [ServerException] for all error codes.
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client? client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTriviaModel> getConreteNumberTrivia(int number) =>
+      _getTriviaFromUrl(
+        "http:numberapi.com/$number",
+      );
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() => _getTriviaFromUrl(
+        "http:numberapi.com/random",
+      );
+
+  Future<NumberTriviaModel> _getTriviaFromUrl(String url) async {
+    final response = await client?.get(
+      url,
+      headers: {"Content-Type": "application/json"},
+    );
+    if (response?.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response!.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
